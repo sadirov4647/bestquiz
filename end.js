5938237220f8ece6587356ce2eed28d33e759f85
sadirov4647 <sadirovizzatillo3@gmail.com>
2021-08-28 13:50:30 +0300

    localstroge

--- end.js
+++ end.js
@@ -3,6 +3,9 @@ const saveScoreBtn = document.querySelector("#save-score-btn");
 const finalScore = document.querySelector("#finalScore");
 const recentScore = localStorage.getItem("recentScore");
 
+
+const highScores = JSON.parse(localStorage.getItem("highScores"));
+console.log(highScores)
 finalScore.innerText = recentScore;
 
 userName.addEventListener("keyup", () => {
@@ -12,5 +15,19 @@ userName.addEventListener("keyup", () => {
 
 
 saveHighScore = e => {
-    e.preventDefault()
+    e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: userName.value,
+    }
+    highScores.push(score);
+
+    highScores.sort((a, b) => {
+        return b.score - a.score
+    })
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
 }
